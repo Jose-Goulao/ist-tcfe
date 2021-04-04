7d58b520247bf8028e1d965789b72aa16a8392c2
Jose-Goulao <jose.goulao@tecnico.ulisboa.pt>
2021-04-04 01:08:52 +0100

    t2.1

--- t2/mat/oct_nos_t2.m
+++ t2/mat/oct_nos_t2.m
@@ -21,7 +21,7 @@ R5 = values(6,3)*1000;
 R6 = values(7,3)*1000;
 R7 = values(8,3)*1000;
 Vs= values(9,3);
-C = values(10,3)*0.000001;
+C1 = values(10,3)*0.000001;
 Kb = values(11,3)*0.001;
 Kd = values(12,3)*1000;
 
@@ -39,17 +39,6 @@ G5 = 1/R5;
 G6 = 1/R6;
 G7 = 1/R7;
 
-printf("%d\n", R1);
-printf("%d\n", R2);
-printf("%d\n", R3);
-printf("%d\n", R4);
-printf("%d\n", R5);
-printf("%d\n", R6);
-printf("%d\n", R7);
-printf("%d\n", Vs);
-printf("%d\n", C);
-printf("%d\n", Kb);
-printf("%d\n", Kd);
 
 
 
@@ -69,7 +58,7 @@ file = fopen("given_vls_m.tex", "w");
 	fprintf(file, "$R6$ & %d \\\\ \\hline \n", R6);
 	fprintf(file, "$R7$ & %d \\\\ \\hline \n", R7);
 	fprintf(file, "$V_s$ & %d \\\\ \\hline \n", Vs);
-	fprintf(file, "$C$ & %d \\\\ \\hline \n", C);
+	fprintf(file, "$C$ & %d \\\\ \\hline \n", C1);
 	fprintf(file, "$K_b$ & %d \\\\ \\hline\n", Kb);
 	fprintf(file, "$K_d$ & %d \\\\ \\hline \n", Kd);
 	
@@ -88,7 +77,7 @@ file = fopen("Dados_ngs-1.txt", "w");
 	fprintf(file, "R7	N8	N7.	%d \n", R7);
 
 	fprintf(file, "Vs	N1	0	%d \n", Vs);
-	fprintf(file, "C	N8	N6	%d \n", C);
+	fprintf(file, "C	N8	N6	%d \n", C1);
 
 	fprintf(file, "Vaux	N7	N7.	0 \n");
 
@@ -99,6 +88,25 @@ fclose(file);
 
 #------------------------------------------------------------
 
+file = fopen("Dados_ngs-1.txt", "w");
+
+	fprintf(file, "R1	N2	N1	%d \n", R1);
+	fprintf(file, "R2	N3	N2	%d \n", R2);
+	fprintf(file, "R3 	N5	N2	%d \n", R3);	
+	fprintf(file, "R4	0	N5	%d \n", R4);
+	fprintf(file, "R5	N5	N6	%d \n", R5);
+	fprintf(file, "R6	0	N7	%d \n", R6);
+	fprintf(file, "R7	N8	N7.	%d \n", R7);
+
+	fprintf(file, "Vs	N1	0	%d \n", Vs);
+	#fprintf(file, "Vx	N8	N6	%d \n", Vx);
+
+	fprintf(file, "Vaux	N7	N7.	0 \n");
+
+	fprintf(file, "G1	N6	N3	N2	N5	%d \n", Kb);
+	fprintf(file, "H1	N5	N8	Vaux	%d \n", Kd);
+	
+fclose(file);
 
 
 
