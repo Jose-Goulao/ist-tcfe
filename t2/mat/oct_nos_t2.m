0fe54dd552305a7afc7e87bf5fbb743e7ab952bb
Jose-Goulao <jose.goulao@tecnico.ulisboa.pt>
2021-04-04 19:52:20 +0100

    ta a aquecer

--- t2/mat/oct_nos_t2.m
+++ t2/mat/oct_nos_t2.m
@@ -2,10 +2,6 @@
 close all
 clear all
 
-#format short e
-#pkg load symbolic
-#pkg load control
-#pkg load signal
 
 #---------------------------------
 # Values from the Python script
@@ -74,36 +70,61 @@ Vn8 = R(7);
 Vb = R(8);
 Vd = R(9);
 Ib = R(10);
-I0 = R(11);
+IVs = R(11);
 Id = R(12);
-Ih = R(13);
+Ih1 = R(13);
 
 Vx = Vn8 - Vn6;
 
-
-
 #############################################################
 #------------------------------------------------------------
-#			files' creation
+#			   oct tbls
 #------------------------------------------------------------
 #############################################################
 
-file = fopen("given_vls_m.tex", "w");
 
-	fprintf(file, "$R1$ & %d \\\\ \\hline \n", R1);
-	fprintf(file, "$R2$ & %d \\\\ \\hline \n", R2);
-	fprintf(file, "$R3$ & %d \\\\ \\hline \n", R3);	
-	fprintf(file, "$R4$ & %d \\\\ \\hline \n", R4);
-	fprintf(file, "$R5$ & %d \\\\ \\hline \n", R5);
-	fprintf(file, "$R6$ & %d \\\\ \\hline \n", R6);
-	fprintf(file, "$R7$ & %d \\\\ \\hline \n", R7);
-	fprintf(file, "$V_s$ & %d \\\\ \\hline \n", Vs);
-	fprintf(file, "$C$ & %d \\\\ \\hline \n", C1);
-	fprintf(file, "$K_b$ & %d \\\\ \\hline\n", Kb);
-	fprintf(file, "$K_d$ & %d \\\\ \\hline \n", Kd);
-	
+file = fopen("Tbl_oct-1.tex", "w");
+	fprintf(file, "$V_{N1}$ & %e \\\\ \\hline \n", Vn1);
+	fprintf(file, "$V_{N2}$ & %e \\\\ \\hline \n", Vn2);
+	fprintf(file, "$V_{N3}$ & %e \\\\ \\hline \n", Vn3);
+	fprintf(file, "$V_{N5}$ & %e \\\\ \\hline \n", Vn5);
+	fprintf(file, "$V_{N6}$ & %e \\\\ \\hline \n", Vn6);
+	fprintf(file, "$V_{N7}$ & %e \\\\ \\hline \n", Vn7);
+	fprintf(file, "$V_{N8}$ & %e \\\\ \\hline \n", Vn8);
+
+	fprintf(file, "$V_{b}$ & %e \\\\ \\hline \n", Vb);
+	fprintf(file, "$V_{d}$ & %e \\\\ \\hline \n", Vd);
+
+	fprintf(file, "$@I_{b}$ & %e \\\\ \\hline \n", Ib);
+	fprintf(file, "$@I_{d}$ & %e \\\\ \\hline \n", Id);
+	fprintf(file, "$@I_{Vs}$ & %e \\\\ \\hline \n", IVs);
+	fprintf(file, "$@I_{H1}$ & %e \\\\ \\hline", Ih1);
 fclose(file);
 
+#------------------------------------------------------------
+
+#############################################################
+#------------------------------------------------------------
+#			   ngs files
+#------------------------------------------------------------
+#############################################################
+#
+#file = fopen("given_vls_m.tex", "w");
+#
+#	fprintf(file, "$R1$ & %d \\\\ \\hline \n", R1);
+#	fprintf(file, "$R2$ & %d \\\\ \\hline \n", R2);
+#	fprintf(file, "$R3$ & %d \\\\ \\hline \n", R3);	
+#	fprintf(file, "$R4$ & %d \\\\ \\hline \n", R4);
+#	fprintf(file, "$R5$ & %d \\\\ \\hline \n", R5);
+#	fprintf(file, "$R6$ & %d \\\\ \\hline \n", R6);
+#	fprintf(file, "$R7$ & %d \\\\ \\hline \n", R7);
+#	fprintf(file, "$V_s$ & %d \\\\ \\hline \n", Vs);
+#	fprintf(file, "$C$ & %d \\\\ \\hline \n", C1);
+#	fprintf(file, "$K_b$ & %d \\\\ \\hline\n", Kb);
+#	fprintf(file, "$K_d$ & %d \\\\ \\hline \n", Kd);
+#	
+#fclose(file);
+#
 #------------------------------------------------------------
 
 file = fopen("Dados_ngs-1.txt", "w");
@@ -139,7 +160,7 @@ file = fopen("Dados_ngs-2.txt", "w");
 	fprintf(file, "R7	N8	N7.	%d \n", R7);
 
 	fprintf(file, "Vs	N1	0	%d \n", Vs);
-	#fprintf(file, "Vx	N8	N6	%d \n", Vx);
+	fprintf(file, "Vx	N8	N6	%d \n", Vx);
 
 	fprintf(file, "Vaux	N7	N7.	0 \n");
 
@@ -148,6 +169,70 @@ file = fopen("Dados_ngs-2.txt", "w");
 	
 fclose(file);
 
+#------------------------------------------------------------
 
+file = fopen("Dados_ngs-3.txt", "w");
+
+	fprintf(file, "R1	N2	N1	%d \n", R1);
+	fprintf(file, "R2	N3	N2	%d \n", R2);
+	fprintf(file, "R3 	N5	N2	%d \n", R3);	
+	fprintf(file, "R4	0	N5	%d \n", R4);
+	fprintf(file, "R5	N5	N6	%d \n", R5);
+	fprintf(file, "R6	0	N7	%d \n", R6);
+	fprintf(file, "R7	N8	N7.	%d \n", R7);
+
+	fprintf(file, "Vs	N1	0	%d \n", Vs);
+	fprintf(file, "C	N8	N6	%d \n", C1);
+
+	fprintf(file, "Vaux	N7	N7.	0 \n");
+
+	fprintf(file, "G1	N6	N3	N2	N5	%d \n", Kb);
+	fprintf(file, "H1	N5	N8	Vaux	%d \n", Kd);
+	
+fclose(file);
+
+#------------------------------------------------------------
+
+file = fopen("Dados_ngs-4.txt", "w");
+
+	fprintf(file, "R1	N2	N1	%d \n", R1);
+	fprintf(file, "R2	N3	N2	%d \n", R2);
+	fprintf(file, "R3 	N5	N2	%d \n", R3);	
+	fprintf(file, "R4	0	N5	%d \n", R4);
+	fprintf(file, "R5	N5	N6	%d \n", R5);
+	fprintf(file, "R6	0	N7	%d \n", R6);
+	fprintf(file, "R7	N8	N7.	%d \n", R7);
+
+	fprintf(file, "Vs	N1	0	%d \n", Vs);
+	fprintf(file, "Vx	N8	N6	%d \n", Vx);
+
+	fprintf(file, "Vaux	N7	N7.	0 \n");
+
+	fprintf(file, "G1	N6	N3	N2	N5	%d \n", Kb);
+	fprintf(file, "H1	N5	N8	Vaux	%d \n", Kd);
+	
+fclose(file);
+
+#------------------------------------------------------------
+
+file = fopen("Dados_ngs-5.txt", "w");
+
+	fprintf(file, "R1	N2	N1	%d \n", R1);
+	fprintf(file, "R2	N3	N2	%d \n", R2);
+	fprintf(file, "R3 	N5	N2	%d \n", R3);	
+	fprintf(file, "R4	0	N5	%d \n", R4);
+	fprintf(file, "R5	N5	N6	%d \n", R5);
+	fprintf(file, "R6	0	N7	%d \n", R6);
+	fprintf(file, "R7	N8	N7.	%d \n", R7);
+
+	fprintf(file, "Vs	N1	0	%d \n", Vs);
+	fprintf(file, "C	N8	N6	%d \n", C1);
+
+	fprintf(file, "Vaux	N7	N7.	0 \n");
+
+	fprintf(file, "G1	N6	N3	N2	N5	%d \n", Kb);
+	fprintf(file, "H1	N5	N8	Vaux	%d \n", Kd);
+	
+fclose(file);
 
 
