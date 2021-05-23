335956cffafe90939c9bc49fa7705234a7471d35
AndruGomes13 <andrepikolim@gmail.com>
2021-05-23 11:20:28 +0000

    dw

--- t4/mat/t4.m
+++ t4/mat/t4.m
@@ -1,4 +1,8 @@
 %gain stage
+clear all
+clc
+close all
+
 
 VT=25e-3
 BFN=178.7
@@ -10,6 +14,9 @@ RB2=20000
 VBEON=0.7
 VCC=12
 RS=100
+C_b = 0.0001
+C_i = 0.0001
+
 
 RB=1/(1/RB1+1/RB2)
 VEQ=RB2/(RB1+RB2)*VCC
@@ -25,23 +32,44 @@ gm1=IC1/VT
 rpi1=BFN/gm1
 ro1=VAFN/IC1
 
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+RSB=RB*RS/(RB+RS)
 
-AV1simple = gm1*RC1/(1+gm1*RE1)
+AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+AVI_DB = 20*log10(abs(AV1))
+AV1simple = RB/(RB+RS) * gm1*RC1/(1+gm1*RE1)
+AVIsimple_DB = 20*log10(abs(AV1simple))
 
 RE1=0
-AV1 = RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
-AV1simple = gm1*RC1/(1+gm1*RE1)
+AV1 = RSB/RS * RC1*(RE1-gm1*rpi1*ro1)/((ro1+RC1+RE1)*(RSB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)
+AVI_DB = 20*log10(abs(AV1))
+AV1simple =  - RSB/RS * gm1*RC1/(1+gm1*RE1)
+AVIsimple_DB = 20*log10(abs(AV1simple))
 
 RE1=100
-
-ZI1 = ((ro1+RC1+RE1)*(RB+rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)
-
-ZX = ro1*((RB+rpi1)*RE1/(RB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RB)+1/RE1+gm1*rpi1/(rpi1+RB)))
-
+ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
+ZX = ro1*((RSB+rpi1)*RE1/(RSB+rpi1+RE1))/(1/(1/ro1+1/(rpi1+RSB)+1/RE1+gm1*rpi1/(rpi1+RSB)))
+ZX = ro1*(   1/RE1+1/(rpi1+RSB)+1/ro1+gm1*rpi1/(rpi1+RSB)  )/(   1/RE1+1/(rpi1+RSB) ) 
 ZO1 = 1/(1/ZX+1/RC1)
 
+n = 70;
+f = logspace(1, 8, n);
+gain_f = zeros(1,n);
 
+for i = 1:n
+  w = f(i)*2*pi;
+  RE1_C = 1/sqrt((1/RE1)**2+(C_b*w)**2);
+  RS_C= sqrt(RS**2 + 1/((w*C_i)**2));
+  RSB_C=RB*RS_C/(RB+RS_C);
+  gain_f(i) = RSB_C/RS_C * RC1*(RE1_C-gm1*rpi1*ro1)/((ro1+RC1+RE1_C)*(RSB_C+rpi1+RE1_C)+gm1*RE1_C*ro1*rpi1 - RE1_C^2);
+endfor
+gain_f_db = 20*log10(abs(gain_f));
+
+
+
+
+RE1=0
+ZI1 = 1/(1/RB+1/(((ro1+RC1+RE1)*(rpi1+RE1)+gm1*RE1*ro1*rpi1 - RE1^2)/(ro1+RC1+RE1)))
+ZO1 = 1/(1/ro1+1/RC1)
 
 %ouput stage
 BFP = 227.3
@@ -53,17 +81,31 @@ IE2 = (VCC-VEBON-VI2)/RE2
 IC2 = BFP/(BFP+1)*IE2
 VO2 = VCC - RE2*IE2
 
-
 gm2 = IC2/VT
 go2 = IC2/VAFP
 gpi2 = gm2/BFP
 ge2 = 1/RE2
 
 AV2 = gm2/(gm2+gpi2+go2+ge2)
-
-
-
 ZI2 = (gm2+gpi2+go2+ge2)/gpi2/(gpi2+go2+ge2)
-
 ZO2 = 1/(gm2+gpi2+go2+ge2)
 
+
+%total
+gB = 1/(1/gpi2+ZO1)
+AV = (gB+gm2/gpi2*gB)/(gB+ge2+go2+gm2/gpi2*gB)*AV1
+AV_DB = 20*log10(abs(AV))
+ZI=ZI1
+ZO=1/(go2+gm2/gpi2*gB+ge2+gB);
+
+gain_final = gain_f*(gB+gm2/gpi2*gB)/(gB+ge2+go2+gm2/gpi2*gB);
+
+gain_final_db = 20*log10(abs(gain_final));
+
+
+hf=figure();
+semilogx(f, gain_final_db);
+
+title("Frequency Response Vo/Vi")
+xlabel("Frequency (Hz)")
+ylabel("Gain (dB)")
